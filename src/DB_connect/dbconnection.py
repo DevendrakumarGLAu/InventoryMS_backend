0d563d9bc53de62e195acb24b6d8bfdd7f9e8a0c
Devendra Kumar Singh <devendra1997kumar@gmail.com>
2024-06-23 12:23:35 +0530

    create file  for .env

--- src/DB_connect/dbconnection.py
+++ src/DB_connect/dbconnection.py
@@ -1,14 +1,29 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
 class Dbconnect:
     @staticmethod
     def dbconnects():
-        connection = mysql.connector.connect(
-            host='localhost',
-            port='3306',
-            user='root',
-            password="Dev@1997",
-            database ="pharmanage"
-        )
+        load_dotenv()
+        host = os.getenv("host")
+        port = os.getenv("port")
+        user = os.getenv("user")
+        password = os.getenv("password")
+        database = os.getenv("database")
 
-        return connection
+        try:
+            connection = mysql.connector.connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                database=database
+            )
+            if connection.is_connected():
+                db_info = connection.get_server_info()
+                print(f"Connected to MySQL Server version {db_info}")
+                return connection
+        except mysql.connector.Error as err:
+            raise err
+
